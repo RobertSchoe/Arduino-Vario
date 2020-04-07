5bedeb3232fd6ddb6d51b293c79b43389dfead7f
Rosch <rob510@gmx.de>
2020-04-07 16:12:07 +0200

    added writing and reading from sd-card
    both static and reading to serial monitor

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,25 +1,43 @@
 /*****************************************************************************
- * Wiring for ESP12e 
- * ESP12e to MS5611
+ * Wiring for ESP12E
+ * ESP12E to MS5611
  * VIN -> VCC
  * GND -> GND
  * D1  -> SCL
  * D2  -> SDA
  * 
+ * BUZZER TO D3 (ESP12e D3 (PIN0 in Arduino IDE))
+ * 
+ * ESP to GY-GPS6MV2
+ * 
+ * ESP to SD (pictures on git-hub)
+ * D8  -> CS
+ * D7  -> MOSI
+ * D6  -> MISO
+ * D5  -> CLK (SCK)
+ * GND -> GND
+ * 3V3 -> Vcc
+ * 
+ * 
  * Wiring for Arduino UNO
+ * UNO to MS5611
  * 5V  -> VCC
  * GND -> GND
  * A5  -> SDA
  * A4  -> SCL
  * 
- * BUZZER TO D3 (ESP12e D3 (PIN0 in Arduino IDE))
+ * BUZZER TO D3
  * 
  *******************************************************************************/
 #include <Wire.h>
 #include <MS5611.h>
+#include <SPI.h>
+#include <SD.h>
 #define BUZZER 0
+#define CS 15
 
 MS5611 sensor(&Wire);
+File myFile;
 
 unsigned long time1 = 0;
 float toneFreq, toneFreqLowpass, pressure, lowpassFast, lowpassSlow ;
@@ -28,13 +46,23 @@ short ddsAcc = 0;
 
 void setup() {
   Serial.begin(9600);
+  Serial.print("Initializing SD card and barometer...");
+  
   pinMode(BUZZER, OUTPUT);
+  
   if(sensor.connect()>0) {
     Serial.println("Error connecting...");
     delay(500);
     setup();
   }
-  ddsAcc = 0;
+  
+  if (!SD.begin(15)) {
+    Serial.println("initialization failed!");
+    while (1);                                          //deadlock and possible restart if needed
+  }
+  Serial.println("initialization done.");
+  printTestToSD();
+  readFromSD();
 }
 
 
@@ -59,11 +87,39 @@ void loop()
   if (toneFreq < 0 || ddsAcc > 0) 
     tone(BUZZER, toneFreq + 510);  
   else
-  {
-    Serial.println("Pause"); 
+  { 
     noTone(BUZZER);
   }
   
   while (millis() < time1);        //loop frequency timer
   time1 += 20;
+}
+
+void printTestToSD(){
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+    if (myFile) {
+      Serial.print("Writing to test.txt...");
+      myFile.println("testing 1, 2, 3.");
+      myFile.close();
+      Serial.println("done.");
+    }
+    else {
+      Serial.println("error opening test.txt");
+    }
+}
+
+void readFromSD(){
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    myFile.close();
+  } 
+  else {
+    Serial.println("error opening test.txt");
+  }
 }
